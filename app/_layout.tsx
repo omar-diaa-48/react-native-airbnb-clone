e77d816a12b8c0bf741285447d89b57bc7e574aa
omar <o.wakeel@intouch.com>
2023-12-23 14:40:10 +0200

    feat: adjust app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
+import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { SplashScreen, Stack, useRouter } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -45,11 +47,43 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  const router = useRouter();
 
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+
+      <Stack.Screen name='(modals)/login' options={{
+        presentation: 'modal',
+        title: 'Log in or sign up',
+        headerTitleStyle: { fontFamily: 'mon-sb' },
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name='close-outline' size={28} />
+          </TouchableOpacity>
+        )
+      }}
+      />
+
+      <Stack.Screen
+        name='listing/[id]'
+        options={{
+          headerTitle: ''
+        }}
+      />
+
+      <Stack.Screen
+        name='(modals)/booking'
+        options={{
+          presentation: 'transparentModal',
+          animation: 'fade',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name='close-outline' size={28} />
+            </TouchableOpacity>
+          )
+        }}
+      />
     </Stack>
   );
 }
