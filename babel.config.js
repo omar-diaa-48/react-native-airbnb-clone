3a13a279861f01e138680f8bd4cc79c5148ff6e2
omar <o.wakeel@intouch.com>
2023-12-23 14:05:33 +0200

    feat: adjust setup base url for the project

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,14 @@ module.exports = function (api) {
     plugins: [
       // Required for expo-router
       'expo-router/babel',
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          '@/*': './*',
+        },
+      }
     ],
   };
 };
