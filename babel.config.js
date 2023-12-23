f9e8af3edec5aed25bdddbc205d40c20c3e1777a
omar <o.wakeel@intouch.com>
2023-12-23 14:08:07 +0200

    feat: remove setup base url for the project

--- babel.config.js
+++ babel.config.js
@@ -4,15 +4,7 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
-      'module-resolver',
-      {
-        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
-        root: ['.'],
-        alias: {
-          '@/*': './*',
-        },
-      }
+      'expo-router/babel'
     ],
   };
 };
